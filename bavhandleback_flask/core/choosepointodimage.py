591f7fb6876f439a5e08602e6c940f24881238d8
zhihaozhao <zhihaozhao@sharkshopping.com>
2020-11-26 09:46:55 +0800

    鼠标选取图像中的像素点

---
+++ bavhandleback_flask/core/choosepointodimage.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+
+# 图片路径
+img = cv2.imread('G:\ghs_Work2018\\bavcloudJoe\\bavhandleback_flask\static\image\img_0000_0.png')
+a = []
+b = []
+
+
+def on_EVENT_LBUTTONDOWN(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        xy = "%d,%d" % (x, y)
+        a.append(x)
+        b.append(y)
+        cv2.circle(img, (x, y), 1, (0, 0, 255), thickness=-1)
+        cv2.putText(img, xy, (x, y), cv2.FONT_HERSHEY_PLAIN,
+                    1.0, (0, 0, 0), thickness=1)
+        cv2.imshow("image", img)
+
+if __name__ == '__main__':
+    cv2.namedWindow("image")
+    cv2.setMouseCallback("image", on_EVENT_LBUTTONDOWN)
+    cv2.imshow("image", img)
+    cv2.waitKey(0)
+    print(a[0], b[0])
